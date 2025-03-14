0192b49ad2b09f95695e79b3393fab9094b0b881
FusiDaniel <fpires.daniel@gmail.com>
2025-03-14 15:44:43 -0300

    refator: remove mobile comment

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -2,7 +2,6 @@ import config from '@repo/tailwind-config';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: [
     './app/**/*.{js,jsx,ts,tsx}',
     '../../packages/ui/src/**/*.{js,jsx,ts,tsx}',
